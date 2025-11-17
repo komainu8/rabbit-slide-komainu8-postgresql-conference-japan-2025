b26971efd1640b6d9459a9afd8cc65aa9ba54769
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-11-17 11:44:42 +0900

    Add the missing extension

--- demo/1-compare-keyword-search-and-semantic-search/create-database.sql
+++ demo/1-compare-keyword-search-and-semantic-search/create-database.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE semantic_search_test;
 \c semantic_search_test
 CREATE EXTENSION IF NOT EXISTS pgroonga;
+CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE jpug_doc_contents (
   title text,

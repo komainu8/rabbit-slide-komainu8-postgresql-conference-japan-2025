4428ded5b42c67c7601d638eb0d673565d515c5d
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-11-18 09:13:18 +0900

    Remove needless scripts

--- demo/compare-key-word-search-and-semantic-search/count-compare-keyword-search-and-semantic-search.sql
+++
@@ -1,8 +0,0 @@
--- COUNT
-SELECT COUNT(*)
-  FROM jpug_doc_contents
- WHERE content &@~ pgroonga_condition('Window関数');
-
-SELECT COUNT(*)
-  FROM jpug_doc_contents
- WHERE content &@* pgroonga_condition('Window関数');

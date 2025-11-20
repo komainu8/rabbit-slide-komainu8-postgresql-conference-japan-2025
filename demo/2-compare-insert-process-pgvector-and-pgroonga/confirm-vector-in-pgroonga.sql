2564ec55a5f0db3519790623ca027d6fbe41a275
Abe Tomoaki <abe@clear-code.com>
2025-11-20 21:25:24 +0900

    demo: Improve confirm-vector-in-pgroonga.sql (#3)
    Dynamically get the table name from the Sources table.

--- demo/2-compare-insert-process-pgvector-and-pgroonga/confirm-vector-in-pgroonga.sql
+++ demo/2-compare-insert-process-pgvector-and-pgroonga/confirm-vector-in-pgroonga.sql
@@ -1,3 +1,20 @@
-SELECT pgroonga_command('dump');
-SELECT pgroonga_command('select --table Sources57344');
-SELECT pgroonga_command('select --table Sources57344')::json->1->0->2;
+SELECT
+  pgroonga_command(
+    'select',
+    ARRAY[
+      'table', t.table_name,
+      'output_columns', 'content,content_code',
+      'output_pretty', 'yes'
+    ])
+FROM (
+  SELECT
+    split_part(cmd, ' ', 2) AS table_name
+  FROM (
+    SELECT
+      regexp_split_to_table(
+        pgroonga_command('dump', ARRAY['dump_records', 'no']),
+        '\n'
+      ) AS cmd
+  ) AS c
+  WHERE cmd LIKE 'table_create%Sources%'
+) AS t;

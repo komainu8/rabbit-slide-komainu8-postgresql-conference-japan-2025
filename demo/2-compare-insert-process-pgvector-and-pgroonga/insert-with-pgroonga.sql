f15f0a68abc24264bba24bc499b23bc11dc4f303
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-11-25 10:24:03 +0900

    Add missing SELECT for confirm the results of INSERT

--- demo/2-compare-insert-process-pgvector-and-pgroonga/insert-with-pgroonga.sql
+++ demo/2-compare-insert-process-pgvector-and-pgroonga/insert-with-pgroonga.sql
@@ -7,3 +7,15 @@ CREATE INDEX pgroonga_content_index ON contents_for_pgroonga
  USING pgroonga (content pgroonga_text_semantic_search_ops_v2)
  WITH (plugins = 'language_model/knn',
        model = 'hf:///groonga/all-MiniLM-L6-v2-Q4_K_M-GGUF');
+
+INSERT INTO contents_for_pgroonga VALUES ('I am a boy');
+INSERT INTO contents_for_pgroonga VALUES ('I am a dog');
+INSERT INTO contents_for_pgroonga VALUES ('I am a king');
+
+SELECT * FROM contents_for_pgroonga;
+--    content   
+-- -------------
+--  I am a boy
+--  I am a dog
+--  I am a king
+-- (3 行)
